df919fa4d5d69e0e3e5df1877311d38b8c94cc78
Leonard Jarkowski <leonard.jarkowski@gmail.com>
2025-07-25 03:23:24 +0200

    added distance aware position initialilzation

--- LJ_gas_run_MD.jl
+++ LJ_gas_run_MD.jl
@@ -1,39 +1,48 @@
 include("LJ_gas.jl")
 
-#using AMDGPU
+using AMDGPU
 using ProgressMeter
+using LinearAlgebra
 
 # system
-n_particles = 1000
-type_A = "Kr"
-mass_A =  83.798             # mass in u = 1e-3 kg/mol
-sigma_A = 0.3627              # sigma in nm     Argon: 0.34
-epsilon_A = 162.58 * Boltzmann     # epsilon in kJ/mol Argon: 120
+n_particles = 500
 
-# see http://www.sklogwiki.org/SklogWiki/index.php/Neon
-type_B = "Ra"
-mass_B =  222             # mass in u = 1e-3 kg/mol
+# LENNARD JONES PARAMS: https://doi.org/10.1080/00268976.2016.1246760
+#type_A = "Kr"
+#mass_A =  83.798             # mass in u = 1e-3 kg/mol
+#sigma_A = 0.3627              # sigma in nm     
+#epsilon_A = 162.58 * R * 1e-3     # epsilon in kJ/mol
+
+type_A = "Ne"
+mass_A =  20.18             # mass in u = 1e-3 kg/mol
+sigma_A = 0.2782              # sigma in nm     
+epsilon_A = 33 * R * 1e-3     # epsilon in kJ/mol
+
+type_B = "Rn"
+mass_B =  222             # mass in u = 1e-3 kg/mol 
 sigma_B= 0.417           # sigma in nm     
-epsilon_B = 300 * Boltzmann   # epsilon in kJ/mol 
+epsilon_B = 300 * R * 1e-3   # epsilon in kJ/mol 
 
 # simulation
-dt = 0.1          # ps
-n_steps = 4000
-temperature = 230     # K
-end_temperature = 100
-box_length = 20      # nm
+dt = 0.002          # ps
+n_steps = 8000000
+temperature = 20     # K
+end_temperature = 20
+box_length = 10      # nm
 tau_thermostat = 1  # thermostat coupling constant in 1/ps
-rij_min = 1e-2      # nm
-NVT = true          # switch to decide between NVT and NVE
+rij_min = 1e-3      # nm
+rij_min_starting = 0.5 # nm
+NVT = false          # switch to decide between NVT and NVE
+equilibrate = true # turn on NVT equilibration for all simulations
 
 # Metadata 
-save_frequency = 2 # save every n-th step
-filename_base = "simulations/JuliaLJFluidTest2"
+save_frequency = 250 # save every n-th step
+filename_base = "simulations/NeRn"
 
 # Switch between CPU and GPU computation by choosing the array type used for initialization
-#ARRAYTPE = ROCArray{Float32} # GPU
+ARRAYTPE = ROCArray{Float32} # GPU
 #ARRAYTPE = Array{Float32} # CPU single precision
-ARRAYTPE = Array{Float64} # CPU double precision
+#ARRAYTPE = Array{Float64} # CPU double precision
 
 #calculate save points
 n_saves = div(n_steps, save_frequency) + 1
@@ -48,6 +57,10 @@ ps = ParticleSystem(n_particles, ARRAYTPE)
 n_A = div(n_particles, 2)
 n_B = n_particles - n_A
 
+box_A = ((0, box_length / 2), (0, box_length), (0, box_length))
+box_B = ((box_length / 2, box_length), (0, box_length), (0, box_length))
+
+
 # fill in the parameters
 
 ps.type[1:n_A] = fill(type_A,n_A)
@@ -61,17 +74,63 @@ ps.mass[n_A+1:end] = fill(mass_B,n_B)
 ps.sigma[n_A+1:end] = fill(sigma_B,n_B)
 ps.epsilon[n_A+1:end] = fill(epsilon_B,n_B)
 
+function get_position(xrange, yrange, zrange)
+    pos = zeros(3)
+    pos[1] = rand(Uniform(xrange...))
+    pos[2] = rand(Uniform(yrange...))
+    pos[3] = rand(Uniform(zrange...))
+    return pos
+end
+
 # initialize LJ params
 initialize_LJ!(ps)
 
-# set initial positions     
-ps.position[1, 1:n_A] = rand(Uniform(0, box_length / 2), n_A)
-ps.position[2, 1:n_A] = rand(Uniform(0, box_length), n_A)
-ps.position[3, 1:n_A] = rand(Uniform(0, box_length), n_A)
+# guess initial position array
+pos_initial = zeros(3, n_particles)
 
-ps.position[1, n_A+1:end] = rand(Uniform(box_length / 2, box_length), n_B)
-ps.position[2, n_A+1:end] = rand(Uniform(0, box_length), n_B)
-ps.position[3, n_A+1:end] = rand(Uniform(0, box_length), n_B)
+for i in 1:n_A
+    pos_initial[:, i] = get_position(box_A...)
+end
+
+for i in n_A+1:n_particles
+    pos_initial[:, i] = get_position(box_B...)
+end
+
+# remove distances which are to small
+for i in 1:100
+    r = [p1[i] - p2[i] for i in 1:3, p1 in eachslice(pos_initial, dims=2), p2 in eachslice(pos_initial, dims=2)]
+    # Apply periodic boundary conditions
+    @. r = r - round(r / box_length) * box_length
+
+    dist = reshape(norm.(eachslice(r,dims=(2, 3))), n_particles^2)
+
+    # set diagonal elements to inf
+    for i in 1:n_particles
+        dist[LinearIndices((n_particles, n_particles))[i,i]] = Inf
+    end
+
+    cutoff = findfirst(sort(dist) .<= rij_min_starting)
+
+    if !isnothing(cutoff)
+        for i in sortperm(dist)[1:cutoff]
+            index = CartesianIndices((n_particles, n_particles))[i][1]
+            if index > n_A
+                pos_initial[:,index] = get_position(box_A...)
+            else
+                pos_initial[:,index] = get_position(box_B...)
+            end
+        end
+    else
+        println("Placed particles respecting rij_min_starting. Current minimum distance: ", minimum(dist))
+        break
+    end
+    
+    i == 100 && println("Could not place all particles with rij_min_starting. Current minimum distance: ", minimum(dist))
+end
+
+# set initial positions     
+ps.position[:, :] = pos_initial
+
 
 # set initial velocities     
 initialize_velocities!(ps, sim.temperature)
@@ -96,19 +155,23 @@ energy_trajectory[3,1] = instantaneous_temperature(ps)    # instantaneous temper
 energy_trajectory[4,1] = ideal_gas_pressure(ps, sim)      # ideal gas pressure
 
 # Equilibrate by simulating a few NVT steps
-for k in 1:1000
-    simulate_NVT_step!(ps, sim)
-    if instantaneous_temperature(ps) < sim.temperature * 1.1
-        println("Rough equilibration finished after $k steps.")
-        break
+if equilibrate
+    sim_equil = SimulationParameters(dt/500, n_steps, temperature, box_length, tau_thermostat, rij_min, Inf, NVT, save_frequency)
+
+    for k in 1:1000
+        simulate_NVT_step!(ps, sim_equil)
+        if instantaneous_temperature(ps) < sim_equil.temperature * 1.1
+            println("Rough equilibration finished after $k steps.")
+            break
+        end
     end
-end
 
-for k in 1:50
-    simulate_NVT_step!(ps, sim)
-end
+    for k in 1:50
+        simulate_NVT_step!(ps, sim_equil)
+    end
 
-println("Equilibration done with current temperature: ", instantaneous_temperature(ps))
+    println("Equilibration done with current temperature: ", instantaneous_temperature(ps))
+end
 
 
 p = Progress(sim.n_steps, dt = 1, showspeed=true)
@@ -133,7 +196,7 @@ stats = @timed for i in 1:n_steps
         energy_trajectory[4,current_save_pos+1] = ideal_gas_pressure(ps, sim)    # ideal gas pressure
 
         if !iszero(end_temperature)
-            @assert end_temperature < temperature
+            @assert end_temperature <= temperature
             T = end_temperature + (temperature - end_temperature) * (1 - div(i,save_frequency) / div(n_steps,save_frequency))
             global sim = SimulationParameters(dt, n_steps, T, box_length, tau_thermostat, rij_min, Inf, NVT, save_frequency)
         end
@@ -143,7 +206,13 @@ stats = @timed for i in 1:n_steps
 end
 
 energies = energy_trajectory[1,:] .+ energy_trajectory[2,:]
+temperatures = energy_trajectory[3,:]
+pressures = energy_trajectory[3,:]
+
 
 write_xyz_trajectory(filename_base, ps, position_trajectory)
 write_energy_trajectory(filename_base, energy_trajectory)
 write_output_file(filename_base,ps,sim,stats.time)
+
+using GLMakie
+lines(energies)
